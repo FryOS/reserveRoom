573345b762dadc54aa7224e195c60c370b8f4720
FryOS <alexeyosp@gmail.com>
2018-04-16 17:59:58 +0300

    настроил роуты (линки) с общего экрана на экран деталей переговорки

--- src/app/components/room-details/room-details.component.ts
+++ src/app/components/room-details/room-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../../domain/room';
+import { RoomService } from '../../service/room.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-room-details',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-details.component.scss']
 })
 export class RoomDetailsComponent implements OnInit {
+  room: Room;
+  constructor(
+    private  service: RoomService,
+    private route: ActivatedRoute
 
-  constructor() { }
+  ) {
+    console.log('constructed');
+  }
 
   ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    if (isNaN(id)){
+      //todo redirect
+      return;
+    }
+    this.room = this.service.getItemById(id);
   }
 
 }
